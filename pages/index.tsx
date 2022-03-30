805d45cb7f597fdb754eec46c5e70e95f59fdad4
Gizem <gizem.candemir@secilstore.com>
2022-03-30 11:53:33 +0300

    Implementing Next.js SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
-const Home: NextPage = () => {
+import {sanityClient, urlFor} from "../sanity"
+import { Post } from '../typings';
+
+interface Props {
+  posts: [Post];
+}
+
+export default function Home ({posts}: Props) {
+  console.log(posts)
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -35,4 +42,24 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+   
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    author-> {
+     name,
+     image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+  const posts = await sanityClient.fetch(query);
+  return{
+    props: {
+      posts,
+    }
+  }
+
+}
