9bd8b5faf28a166bd47551a19da61a424edf8069
Gizem <gizem.candemir@secilstore.com>
2022-03-30 10:36:01 +0300

    Done header!

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import Head from 'next/head'
 import Header from '../components/Header'
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Medium 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<Header />
+     <Header />
    </div>
   )
 }
